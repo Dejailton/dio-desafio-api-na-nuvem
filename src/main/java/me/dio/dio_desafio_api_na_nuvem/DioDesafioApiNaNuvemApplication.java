61ce95da30f95fbb85dbd41608688471cf4f3ed4
Dejailton <devdejailton@gmail.com>
2024-08-17 20:39:19 -0300

    Initial commit

---
+++ src/main/java/me/dio/dio_desafio_api_na_nuvem/DioDesafioApiNaNuvemApplication.java
@@ -0,0 +1,13 @@
+package me.dio.dio_desafio_api_na_nuvem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioDesafioApiNaNuvemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioDesafioApiNaNuvemApplication.class, args);
+	}
+
+}
